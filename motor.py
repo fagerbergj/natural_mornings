99409dfb401409f589e3df5972b6db2b3a067fd6
Francesca <matiraf@wit.edu>
2019-04-08 13:28:39 -0400

    2 motor motoring

--- motor.py
+++ motor.py
@@ -5,20 +5,33 @@ GPIO.setmode(GPIO.BOARD)
  
 Motor1A = 16
 Motor1B = 18
-Motor1E = 22
+Motor1E = 
+
+Motor2A = 23
+Motor2B = 21
+Motor2E = 19
  
 GPIO.setup(Motor1A,GPIO.OUT)
 GPIO.setup(Motor1B,GPIO.OUT)
 GPIO.setup(Motor1E,GPIO.OUT)
+
+GPIO.setup(Motor2A,GPIO.OUT)
+GPIO.setup(Motor2B,GPIO.OUT)
+GPIO.setup(Motor2E,GPIO.OUT)
  
 print ("Turning motor on")
 GPIO.output(Motor1A,GPIO.HIGH)
 GPIO.output(Motor1B,GPIO.LOW)
 GPIO.output(Motor1E,GPIO.HIGH)
+
+GPIO.output(Motor2A,GPIO.HIGH)
+GPIO.output(Motor2B,GPIO.LOW)
+GPIO.output(Motor2E,GPIO.HIGH)
  
 sleep(5)
  
 print ("Stopping motor")
 GPIO.output(Motor1E,GPIO.LOW)
+GPIO.output(Motor2E,GPIO.LOW)
  
 GPIO.cleanup()
