130b46b05cb17812dac3c0ec10673d1562f7c134
Francesca <matiraf@wit.edu>
2019-04-08 13:42:38 -0400

    plz y??

--- clock.py
+++ clock.py
@@ -7,21 +7,6 @@ import numpy as np
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-Motor1A = 16
-Motor1B = 18
-Motor1E = 22
-
-Motor2A = 23
-Motor2B = 21
-Motor2E = 19
-
-GPIO.setup(Motor1A,GPIO.OUT)
-GPIO.setup(Motor1B,GPIO.OUT)
-GPIO.setup(Motor1E,GPIO.OUT)
-
-GPIO.setup(Motor2A,GPIO.OUT)
-GPIO.setup(Motor2B,GPIO.OUT)
-GPIO.setup(Motor2E,GPIO.OUT)
 
 def average_light_value (seconds=60):
     pin_to_circuit = 7
@@ -79,6 +64,23 @@ with open('config.json') as json_file:
             nowTimeInMin = dateTimeNow.minute + dateTimeNow.hour * 60
             activated = day['time'] == nowTimeInMin
         if activated:
+            
+            Motor1A = 16
+            Motor1B = 18
+            Motor1E = 22
+
+            Motor2A = 23
+            Motor2B = 21
+            Motor2E = 19
+
+            GPIO.setup(Motor1A,GPIO.OUT)
+            GPIO.setup(Motor1B,GPIO.OUT)
+            GPIO.setup(Motor1E,GPIO.OUT)
+
+            GPIO.setup(Motor2A,GPIO.OUT)
+            GPIO.setup(Motor2B,GPIO.OUT)
+            GPIO.setup(Motor2E,GPIO.OUT)
+            
             if day["playSound"]:
                 # play sound
                 print('Play sound')
