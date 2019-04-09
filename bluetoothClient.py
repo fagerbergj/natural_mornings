8cc702abb715fe2f3be1ba8650b5c881331187ac
jasonfagerberg-toast <jason.fagerberg@toasttab.com>
2019-04-08 21:18:40 -0400

    test

--- bluetoothClient.py
+++ bluetoothClient.py
@@ -40,8 +40,8 @@ GPIO.setup(17, GPIO.OUT)
 #	time.sleep(1)
 
 
-server_sock = bluetooth.BluetoothSocket(RFCOMM)
-server_sock.bind(("", PORT_ANY))
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+server_sock.bind(("", bluetooth.PORT_ANY))
 server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
@@ -50,8 +50,8 @@ uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
 advertise_service(server_sock, "AquaPiServer",
                   service_id=uuid,
-                  service_classes=[uuid, SERIAL_PORT_CLASS],
-                  profiles=[SERIAL_PORT_PROFILE],
+                  service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                  profiles=[bluetooth.SERIAL_PORT_PROFILE],
                   #                   protocols = [ OBEX_UUID ]
                   )
 while True:
