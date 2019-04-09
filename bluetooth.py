596c0f911566dec44846e32cb6ca78e4dd9a69a7
jasonfagerberg-toast <jason.fagerberg@toasttab.com>
2019-04-08 21:03:36 -0400

    test

--- bluetooth.py
+++ bluetooth.py
@@ -2,10 +2,10 @@ import os
 import glob
 import time
 import RPi.GPIO as GPIO
-from bluetooth import *
+import bluetooth
 
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
+# os.system('modprobe w1-gpio')
+# os.system('modprobe w1-therm')
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
